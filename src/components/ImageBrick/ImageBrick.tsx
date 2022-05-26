9b4a846babd3550f13b650e9eec70d6bd60821ca
Anton Mikhalev <compromisedbeacon+gh@gmail.com>
2022-05-27 00:16:22 +0300

    Initial import

---
+++ src/components/ImageBrick/ImageBrick.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ImageBrick.css';
+
+export default function ImageBrick() {
+    return <div className="rounded border-blue-700">
+        Hello, World!
+    </div>
+}
